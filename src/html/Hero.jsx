b52941be4beae60ddf8692bbbfb766994afdae30
syed momin <syedmomin168@gmail.com>
2024-11-17 09:17:32 +0500

    changes done

--- src/html/Hero.jsx
+++ src/html/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
 
     const springs = useSpring({
         from: {
-            y : -100
+            y: -100
         },
         to: {
             y: 0
@@ -13,34 +13,34 @@ const Hero = () => {
     })
 
     return (
-        <animated.div className='hero-container' style={{...springs}}>
+        <animated.div className='hero-container' style={{ ...springs }}>
 
             <div className="text-container">
                 <div className="text-box">
-                    <h1>Hongqi </h1>
-                    <h3>Make sure to keep the brightness at maximum to experience the full gloss </h3>
+                    <h1>Dream</h1>
+                    <h3>Today, I created this modal. Very soon, this dream of mine will come true.</h3>
                 </div>
             </div>
             <div className="icon-container">
                 <div className="icon-box">
-                    <a href="https://github.com/laMuJer" target={'_blank'}>
+                    <a href="https://github.com/syedmomin" target={'_blank'}>
                         <img src="/github.svg" alt="github" />
                     </a>
-                    <a href="https://www.facebook.com/staphy.staphy.9/" target={'_blank'}>
+                    <a href="https://www.facebook.com/DanySAM.hacki.786" target={'_blank'}>
                         <img src="/facebook.svg" alt="facebook" />
                     </a>
-                    <a href="https://twitter.com/racheldarcy365" target={'_blank'}>
-                        <img src="/twitter.svg" alt="twitter" />
+                    <a href="https://www.linkedin.com/in/syedabdulmomin/" target={'_blank'}>
+                        <img src="/linkedin.svg" alt="linkedin" />
                     </a>
                 </div>
             </div>
-            <div className="credit-container">
+            {/* <div className="credit-container">
                 <div className="credit-box">
                     This work is based on
                     <a href="https://sketchfab.com/3d-models/hongqi-h9-bbcbdc697a614c25a5b02ce2a4b2c6ca" target={'_blank'}> hongqi h9</a> by
                     <a href="https://sketchfab.com/a0930582398" target={'_blank'}> Davidson</a>
                 </div>
-            </div>
+            </div> */}
         </animated.div>
     )
 }
